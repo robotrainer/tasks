023c748e563566c51c92e225e08d66dd3bbd419d
robotrainer <asbu26@gmail.com>
2020-05-13 20:46:39 +0700

    Исправил. Пришлось добавить новую переменную.

--- lesson7/array_7.9.js
+++ lesson7/array_7.9.js
@@ -1,16 +1,19 @@
 let a = [1, 5, 6, 7, 8, 0, 1, 2, 1, 1, 7, 10, 11, 0];
-let n = 1;
+let n = 0;
 let length = 1;
+let max = 0;
 
 for(let i = 0; i < a.length; i++){
-  if(a[i-1] < a[i]){
+  if(a[i] > max){
     n++;
+    max = a[i];
     if(n > length){
       length = n;
     }
   }
   else{
-    n = 1;
+    n = 0;
+    max = 0;
   }
 }
 
