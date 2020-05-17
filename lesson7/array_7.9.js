ca1818e2dc8ce0469330c688e091154ef733bb8e
robotrainer <asbu26@gmail.com>
2020-05-17 16:03:56 +0700

    Исправил

--- lesson7/array_7.9.js
+++ lesson7/array_7.9.js
@@ -1,19 +1,16 @@
 let a = [1, 5, 6, 7, 8, 0, 1, 2, 1, 1, 7, 10, 11, 0];
 let n = 0;
 let length = 1;
-let max = 0;
 
 for(let i = 0; i < a.length; i++){
-  if(a[i] > max){
+  if(i === 0 || a[i] > a[i-1]){
     n++;
-    max = a[i];
     if(n > length){
       length = n;
     }
   }
   else{
     n = 0;
-    max = 0;
   }
 }
 
