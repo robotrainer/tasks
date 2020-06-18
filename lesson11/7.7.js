6c70d627ba00271c5b1a8cfb8b9be22aecf3205b
robotrainer <asbu26@gmail.com>
2020-06-18 20:52:47 +0900

    Исправил

--- lesson11/7.7.js
+++ lesson11/7.7.js
@@ -2,6 +2,9 @@ const _ = require('lodash');
 
 let a = [7, 2, 3, 0];
 
-[a[a.indexOf(_.min(a))], a[a.indexOf(_.max(a))]] = [a[a.indexOf(_.max(a))], a[a.indexOf(_.min(a))]];
+let min = a.indexOf(_.min(a));
+let max = a.indexOf(_.max(a));
+
+[a[min], a[max]] = [a[max], a[min]];
 
 console.log(a);
