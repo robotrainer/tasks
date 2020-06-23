131cb2b42605fd335e63e410322b3d7d75a04b01
robotrainer <asbu26@gmail.com>
2020-06-24 00:04:51 +0900

    Исправил.
    Получилось с кастылём.

--- lesson11/8.10.js
+++ lesson11/8.10.js
@@ -1,21 +1,24 @@
 const readlineSync = require("readline-sync");
 
-let str = readlineSync.question('Введите слово:\n');
+let str = readlineSync.question('Введите выражение:\n');
 
-const re = /([+\-*/|**])?(\d+)/g;
+const re = /([+\-*/]|[**])?(\d+)/g;
 
 let match;
 let result = 0;
 
 while(match !== null){
   match = re.exec(str);
-  const op = match[1] || '+';
-  const num = parseInt(match[2]);
-  result = op === '+' ? result + num:
-           op === '*' ? result * num:
-           op === '/' ? result / num:
-           op === '-' ? result - num:
-                        result ** num;
+  console.log(match);
+  if(match !== null){
+    const op = match[1] || '+';
+    const num = parseInt(match[2]);
+    result = op === '+' ? result + num:
+             op === '*' ? result * num:
+             op === '/' ? result / num:
+             op === '-' ? result - num:
+                          result ** num;   
+  }
 }
 
 console.log(result);
